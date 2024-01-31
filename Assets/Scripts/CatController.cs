f32267298d2fad8973b25aa46e1f39cfb120aeeb
smilejsu <smilejsu82@gmail.com>
2024-01-31 18:53:40 +0900

    ClimbCloud 완료

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatController : MonoBehaviour
 {
@@ -11,8 +12,14 @@ public class CatController : MonoBehaviour
     [SerializeField]
     private ClimbCloudGameDirector gameDirector;
 
+    private Animator anim;
+
     private void Start()
     {
+        //this.gameObject.GetComponent<Animation>();
+
+        anim = GetComponent<Animator>();
+
         //this.gameDirector = GameObject.Find("GameDirector").GetComponent<ClimbCloudGameDirector>();
         //this.gameDirector = GameObject.FindAnyObjectByType<ClimbCloudGameDirector>();
     }
@@ -20,12 +27,12 @@ public class CatController : MonoBehaviour
     void Update()
     {
         //스페이스바를 누르면 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    //힘을 가한다 
-        //    this.rbody.AddForce(this.transform.up * this.force);
-        //    //this.rbody.AddForce(Vector3.up * this.force);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //힘을 가한다 
+            this.rbody.AddForce(this.transform.up * this.jumpForce);
+            //this.rbody.AddForce(Vector3.up * this.force);
+        }
 
         // -1, 0, 1 : 방향 
         int dirX = 0;
@@ -40,7 +47,7 @@ public class CatController : MonoBehaviour
             dirX = 1;
         }
 
-        Debug.Log(dirX); //방향 -1, 0, 1
+        //Debug.Log(dirX); //방향 -1, 0, 1
 
         //스케일 X를 변경 하는데 키가 눌렸을 때만 
         //키가 눌렸을때만 = (dirX != 0)
@@ -50,7 +57,7 @@ public class CatController : MonoBehaviour
         
 
         //벡터의 곱 
-        Debug.Log(this.transform.right * dirX);  //벡터3
+        //Debug.Log(this.transform.right * dirX);  //벡터3
 
         //도전 ! : 속도를 제한하자 
         //velocity.x 가 3정도가 넘어가니깐 빨라지는거 같드라구...
@@ -59,6 +66,18 @@ public class CatController : MonoBehaviour
             this.rbody.AddForce(this.transform.right * dirX * moveForce);
         }
 
+        this.anim.speed = (Mathf.Abs(this.rbody.velocity.x) / 2f);
         this.gameDirector.UpdateVelocityText(this.rbody.velocity);
+        this.gameDirector.UpdateIsJumpingText(this.rbody.velocity);
+    }
+
+    //Trigger모드일경우 충돌 판정을 해주는 이벤트 함수 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //최초 충돌 할때 한번만 호출 
+        Debug.LogFormat("OnTriggerEnter2D: {0}", collision);
+
+        //장면을 전환 
+        SceneManager.LoadScene("ClimbCloudClear");
     }
 }
--- Assets/Scripts/ClimbCloudGameDirector.cs
+++ Assets/Scripts/ClimbCloudGameDirector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,17 @@ using UnityEngine.UI;
 public class ClimbCloudGameDirector : MonoBehaviour
 {
     [SerializeField] private Text velocityText;
+    [SerializeField] private Text isJumpingText;
 
     public void UpdateVelocityText(Vector2 velocity)
     {
         float velocityX = Mathf.Abs(velocity.x);
         this.velocityText.text = velocityX.ToString();
+    }
+
+    public void UpdateIsJumpingText(Vector2 velocity)
+    {
+        bool isJumping = (Mathf.Abs(velocity.y) == 0) ? false : true;
+        this.isJumpingText.text = isJumping.ToString();
     }
 }
